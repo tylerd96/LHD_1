080c8df7e54afa3dd87a372c64b178463f947c8f
davistj7 <davistj7@miamioh.edu>
2016-12-03 10:58:42 -0500

    character start

--- PacMan/src/Character.java
+++ PacMan/src/Character.java
@@ -1,4 +1,37 @@
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 public class Character {
-
+	
+	private int x,y,dx,dy;
+	private final int SIZE = 24;
+	private Color color;
+	private Image image;
+	
+	public Character(int x, int y, Color color, String image) {
+		setX(x);
+		setY(y);
+		setColor(color);
+		setImage(image);
+	}
+	
+	
+	
+	//================================================ Getters / Setters
+	public int getX() 					{return x;}
+	public void setX(int x)				{this.x = x;}
+	public int getY() 					{return y;}
+	public void setY(int y) 			{this.y = y;}
+	public int getDx() 					{return dx;}
+	public void setDx(int dx) 			{this.dx = dx;}
+	public int getDy() 					{return dy;}
+	public void setDy(int dy) 			{this.dy = dy;}
+	public Color getColor() 			{return color;}
+	public void setColor(Color color) 	{this.color = color;}
+	public void setImage(String imageFile) {
+		image = Toolkit.getDefaultToolkit().getImage("GameImages/" + imageFile);
+	}
+	
+	
 }
--- PacMan/src/Pacman.java
+++ PacMan/src/Pacman.java
@@ -1,4 +1,11 @@
+import java.awt.Color;
 
 public class Pacman extends Character {
 
+	public Pacman(int x, int y, Color color, String image) {
+		super(x, y, color, image);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 }
